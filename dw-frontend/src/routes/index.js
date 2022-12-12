442ae0a0f7ef514e754553292028cc141cdb0b81
Juan Manuel Rassa <jmrassaproduccion@gmail.com>
2022-12-11 23:59:33 -0500

    Deleting unused 'cart' route, context and component/page.

--- dw-frontend/src/routes/index.js
+++ dw-frontend/src/routes/index.js
@@ -3,7 +3,6 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import UserAuthContext from '../context/auth';
 
-import Cart from '../pages/Cart';
 import Home from '../pages/Home';
 import Login from '../pages/Login';
 
@@ -13,7 +12,6 @@ function Routing() {
   return (
     <Routes>
       <Route path="/" element={authState.isLoggedIn ? <Home /> : <Navigate replace to="/login" /> } />
-      <Route path="/cart" element={<Cart />} />
       <Route path="/login" element={authState.isLoggedIn ? <Navigate replace to="/" />  : <Login /> } />
     </Routes>
   );
