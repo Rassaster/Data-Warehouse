ff6de61c87989f3030a9b735be8275e876ede637
Juan Manuel Rassa <jmrassaproduccion@gmail.com>
2022-12-12 00:13:23 -0500

    FE - Contacts: Creation of Contacts' route and component.

---
+++ dw-frontend/src/pages/Contacts/index.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect, useRef, useContext } from 'react';
+import api from '../../services/api';
+import UserAuthContext from '../../context/auth';
+import './styles.css'
+
+// Global Constants for API Request:
+const BASE_URL = "http://localhost:3000/dataWarehouse";
+const GET_ALL_CONTACT = "/contacts/listAll";
+const CREATE_CONTACT = "/contacts/create";
+const UPDATE_CONTACT = "contacts/updateContactId:";
+const DELETE_CONTACT = "/contacts/deleteContactId:";
+
+function Contacts() {
+  // Declaration of Global Auth Context
+  const { authState, setAuthState } = useContext(UserAuthContext);
+
+  return (
+    <h1>Contacts</h1>
+  )
+}
+
+export default Contacts;
